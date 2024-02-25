2c2a54faee7d3b56bd69def5dc4344674781956a
Giorgi Ghurtskaia <gghurtskaia@gmail.com>
2024-02-25 14:37:51 +0400

    Create task_2.py

---
+++ task_2.py
@@ -0,0 +1,12 @@
+import datetime
+
+today = datetime.date.today()
+year = today.year
+
+name = input("შეიყვანეთ თქვენი სახელი: ")
+surname = input("შეიყვანეთ თქვენი გვარი: ")
+birth_year = int(input("შეიყვანეთ თქვენი დაბადების წელი: "))
+
+age = year - birth_year
+
+print("გამარჯობა,", name, surname, "თქვენ ხართ:", age, "წლის")
